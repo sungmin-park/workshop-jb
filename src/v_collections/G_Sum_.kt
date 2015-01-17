e00de3596a265ae2d1d9ccf14227fa7722da8f55
Sung Min Park <vamf12@gmail.com>
2015-01-17 17:46:05 +0900

    pass v_G

--- src/v_collections/G_Sum_.kt
+++ src/v_collections/G_Sum_.kt
@@ -4,8 +4,4 @@ fun example6() {
     val sum = listOf(1, 5, 3).sum()
 }
 
-fun Customer.getTotalOrderPrice(): Double {
-    // Returns the sum of prices of all products ordered by customer.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
+fun Customer.getTotalOrderPrice(): Double = orders.flatMap({ it.products }).map({ it.price }).sum()
