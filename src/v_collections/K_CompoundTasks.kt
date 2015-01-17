958c2b58a250ddd0fedab74ec9f761efe33c093d
Sung Min Park <vamf12@gmail.com>
2015-01-17 21:08:58 +0900

    pass v_K

--- src/v_collections/K_CompoundTasks.kt
+++ src/v_collections/K_CompoundTasks.kt
@@ -1,18 +1,25 @@
 package v_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { product in it.orderedProducts }.toSet()
 }
+
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive among delivered products
     // (use Order.isDelivered flag)
-    todoCollectionTask()
+    return orders
+            .filter { it.isDelivered }
+            .flatMap { it.products }
+            .sortBy { it.price }
+            .firstOrNull()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Returns number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers
+            .flatMap { it.orders }
+            .flatMap { it.products }
+            .count { it == product }
 }
