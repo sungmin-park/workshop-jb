63f79aed29a84876a36679bb8e79f47780991a97
Sung Min Park <vamf12@gmail.com>
2015-01-17 20:58:44 +0900

    pass v_J

--- src/v_collections/J_Fold_.kt
+++ src/v_collections/J_Fold_.kt
@@ -1,7 +1,5 @@
 package v_collections
 
-import java.util.HashSet
-
 fun example9() {
     val product = listOf(1, 2, 3, 4).fold(1, { partProduct, element -> element * partProduct })
     product == 24
@@ -10,14 +8,13 @@ fun example9() {
 // The same as
 fun whatFoldDoes(): Int {
     var product = 1
-    listOf(1, 2, 3, 4).forEach { element -> product = element * product}
+    listOf(1, 2, 3, 4).forEach { element -> product = element * product }
     return product
 }
 
 fun Shop.getProductsOrderedByAllCustomers(): Set<Product> {
     // Return set of products ordered by every customer
     return customers.fold(allOrderedProducts, {
-        orderedByAll, customer ->
-        todoCollectionTask()
+        orderedByAll, customer -> orderedByAll.filter({ it in customer.orderedProducts }).toSet()
     })
 }
