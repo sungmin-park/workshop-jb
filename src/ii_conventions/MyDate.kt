0f2890375b9d17960aaea573caa81e29b43ea8c6
Sung Min Park <vamf12@gmail.com>
2015-01-17 15:23:04 +0900

    pass 11

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,6 +1,18 @@
 package ii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+    fun compareTo(date: MyDate): Int {
+        val yearDiff = year - date.year
+        if (yearDiff != 0) {
+            return yearDiff
+        }
+        val month = month - date.month
+        if (month != 0) {
+            return month
+        }
+        return dayOfMonth - date.dayOfMonth
+    }
+}
 
 enum class TimeInterval {
     DAY
