cc840481c1f3e8856e653c0a07fd895298098b6e
Sung Min Park <vamf12@gmail.com>
2015-01-17 14:44:43 +0900

    pass 02

--- src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParamsUsage.kt
+++ src/i_introduction/_2_Default_And_Named_Params/DefaultAndNamedParamsUsage.kt
@@ -4,14 +4,17 @@ import util.TODO
 import i_introduction._1_Functions.task1
 
 fun todoTask2_2() = TODO(
-    """
+        """
         Task 2(2).
         Implement the same logic as in 'task1' again through the library method 'joinToString()'.
         Change values of some arguments of 'joinToString'.
     """,
-    references = { (collection: Collection<Int>) -> task1(collection); collection.joinToString() })
+        references = {(collection: Collection<Int>) -> task1(collection); collection.joinToString() })
+
+public fun <T> String.join(collection: Collection<T>, separator: String = ", "): String {
+    return collection.joinToString(separator = separator);
+}
 
 fun task2_2(collection: Collection<Int>): String {
-    todoTask2_2()
-    return collection.joinToString()
+    return "{${", ".join(collection)}}"
 }
