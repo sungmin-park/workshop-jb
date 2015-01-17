f5b6b5e74f9fe3065aa5a3f679b0c508fc9218e9
Sung Min Park <vamf12@gmail.com>
2015-01-17 16:51:40 +0900

    pass 19

--- src/iii_properties/_19_LazyProperty.kt
+++ src/iii_properties/_19_LazyProperty.kt
@@ -2,12 +2,25 @@ package iii_properties
 
 import util.TODO
 
-class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask19()
+class once<T>(val func: () -> T) {
+    var value: T? = null
+
+    fun invoke(): T {
+        if (value == null) {
+            value = func()
+        }
+        return value!!
+    }
+}
+
+class LazyProperty(initializer: () -> Int) {
+    val initializer = once(initializer)
+    val lazy: Int
+        get() = initializer()
 }
 
 fun todoTask19() = TODO(
-    """
+        """
         Task 19.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by 'initializer()' invocation
@@ -15,5 +28,5 @@ fun todoTask19() = TODO(
         You can add as many additional properties as you need.
         Do not use Delegates ;).
     """,
-    references = { LazyProperty({ 42 }).lazy }
+        references = { LazyProperty({ 42 }).lazy }
 )
